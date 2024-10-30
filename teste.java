d7add43fe65d53b8efde870530a99e52846ba04f
RogerioPereira22 <rogeriojr2116@gmail.com>
2024-10-30 18:18:32 -0300

    tratativa de tipos de numeros verifica qual tipo de numero é

--- teste.java
+++ teste.java
@@ -1,10 +1,10 @@
 int main(){
     int a ,b;
     float f;
-    f = 3.14;
-    system.out.print("algo \n", 3.14,b);
+    f = 3.14hg;
+    system.out.print("algo \n", 7.8444,b);
     system.in.scan(int, a);
-    while (a < 10){
+    while (a <= 10){
         if( b > a){
             break;
 
